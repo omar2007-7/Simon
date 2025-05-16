5a6ea1de421ca7d999e8d21a7aa6d269ea801ea0
omar choujaa <omarchoujaa2007325@gmail.com>
2025-05-16 23:21:12 +0100

    Simon game

---
+++ script.js
@@ -0,0 +1,83 @@
+const squares = $(".squares");
+const start = $("#start");
+
+const h = $("h1");
+const body = $("body");
+const p = $("#p");
+
+let sequence = [];
+let mySequence = [];
+let i = 0;
+let level = 0;
+const COLOR = ["blue", "purple", "yellow", "orange"];
+start.click(function () {
+    starts();
+    sequences();
+    h.text("Level : " + level);
+});
+
+function buttonClick() {
+    squares.click(function () {
+        const th = $(this);
+        const color = $(this).attr("id");
+        mySequence.push(color);
+
+        if (mySequence[i] === sequence[i]) {
+            playAudio(mySequence[i]);
+            th.addClass("right");
+            setTimeout(() => {
+                th.removeClass("right");
+            }, 100);
+            i++;
+            if (i >= sequence.length) {
+                nextLevel();
+                console.log(" if right and i>" + level);
+                sequences();
+                h.text("Level : " + level);
+            }
+        } else {
+            playAudio("wrong");
+            th.addClass("wrong");
+            body.addClass("bad");
+            setTimeout(() => {
+                th.removeClass("wrong");
+                body.removeClass("bad");
+            }, 100);
+            starts();
+            h.text("Level : " + level);
+            setTimeout(() => {
+                sequences();
+            }, 500);
+        }
+    });
+}
+
+function sequences() {
+    const randNum = Math.floor(Math.random() * 4);
+    const randColor = COLOR[randNum];
+    sequence.push(randColor);
+    const q = $(`#${randColor}`);
+    q.fadeOut();
+    q.fadeIn();
+}
+
+function nextLevel() {
+    mySequence = [];
+    level++;
+    i = 0;
+}
+
+function starts() {
+    sequence = [];
+    mySequence = [];
+    i = 0;
+    level = 0;
+    p.text("if something goes wrong click start.")
+    squares.off("click");
+    buttonClick();
+}
+
+function playAudio(color) {
+    const a = new Audio(`./sounds/${color}.mp3`);
+    a.play();
+}
